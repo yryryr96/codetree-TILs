d7e5f3b3c005d5fa64f4892827820e67d4a364e9
yryryr96 <122415720+yryryr96@users.noreply.github.com>
2024-04-08 23:40:41 +0900

    Title: 포탑 부수기, Time: 556ms, Memory: 22MB, Status: Passed - CodeTree

--- 240408/포탑 부수기/destroy-the-turret.java
+++ 240408/포탑 부수기/destroy-the-turret.java
@@ -5,7 +5,6 @@ public class Main {
 
     static int N,M,K;
     static int[][] map, turn;
-    static boolean[][] live;
     static class Pair {
         int y, x, power;
         Pair (int y, int x, int power) {
@@ -34,18 +33,11 @@ public class Main {
         K = stoi(st.nextToken());
         map = new int[N][M];
         turn = new int[N][M];
-        live = new boolean[N][M];
-
-        // 살아있는 포탑 초기화
-        for (int i = 0; i < N; i++) {
-            Arrays.fill(live[i], true);
-        }
 
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < M; j++) {
                 map[i][j] = stoi(st.nextToken());
-                if (map[i][j] == 0) live[i][j] = false;
             }
         }
 
@@ -70,20 +62,13 @@ public class Main {
     }
 
     static void simulate(int t) {
-//        System.out.println("t = " + t);
         Pair attacker = selectAttacker();
         Pair target = selectTarget(attacker);
 
-//        System.out.println("attacker.y = " + attacker.y + " attacker.x = " + attacker.x + " power = " + attacker.power);
-//        System.out.println("target.y = " + target.y + " target.x = " + target.x);
         affected = new boolean[N][M];
         if (!laserAttack(attacker, target)) throwAttack(attacker, target);
         turn[attacker.y][attacker.x] = t;
         repair();
-//
-//        for (int i = 0; i < N; i++) {
-//            System.out.println(Arrays.toString(map[i]));
-//        }
     }
 
     static void repair() {
@@ -118,7 +103,6 @@ public class Main {
         while(!q.isEmpty()) {
 
             Pair now = q.poll();
-//            System.out.println("now.y = " + now.y + " now.x = " + now.x);
             if (now.y == target.y && now.x == target.x) {
                 map[now.y][now.x] -= attacker.power;
                 temp = true;
@@ -133,8 +117,6 @@ public class Main {
                 else if (ny < 0) ny = N-1;
                 else if (nx >= M) nx = 0;
                 else if (nx < 0) nx = M-1;
-//                System.out.println("ny = " + ny + " nx = " + nx);
-//                System.out.println("visited[ny][nx] = " + visited[ny][nx]);
                 if (map[ny][nx] > 0 && visited[ny][nx] == null) {
                     visited[ny][nx] = new Pair(now.y, now.x);
                     q.add(new Pair(ny,nx));
@@ -143,7 +125,6 @@ public class Main {
         }
 
         if (temp) {
-//            System.out.println("laser");
             q.clear();
             q.add(visited[target.y][target.x]);
             affected[target.y][target.x] = true;
@@ -164,7 +145,6 @@ public class Main {
 
     static void throwAttack(Pair attacker, Pair target) {
 
-//        System.out.println("throw");
         map[target.y][target.x] -= attacker.power;
 
         affected[attacker.y][attacker.x] = true;
@@ -193,7 +173,6 @@ public class Main {
             if (ny == attacker.y && nx == attacker.x) continue;
 
             if (map[ny][nx] > 0) {
-//                System.out.println("ny = " + ny + " nx = " + nx);
                 map[ny][nx] -= (int) (attacker.power/2);
                 affected[ny][nx] = true;
             }
@@ -252,9 +231,6 @@ public class Main {
             }
         });
 
-//        for (int i = 0; i < targets.size(); i++) {
-//            System.out.println("targets.get(i).power = " + targets.get(i).power);
-//        }
         return targets.get(0);
     }
 
