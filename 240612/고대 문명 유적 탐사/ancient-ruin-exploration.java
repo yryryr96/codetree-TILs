94d9a86c769edde5b0c0be30bd3ba577889b31a9
yryryr96 <122415720+yryryr96@users.noreply.github.com>
2024-06-12 14:29:02 +0900

    Title: 고대 문명 유적 탐사, Time: 92ms, Memory: 9MB, Status: Wrong Answer - CodeTree

--- 240612/고대 문명 유적 탐사/ancient-ruin-exploration.java
+++ 240612/고대 문명 유적 탐사/ancient-ruin-exploration.java
@@ -1,160 +1,185 @@
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Scanner;
+import java.util.*;
+import java.io.*;
 
 public class Main {
-    static final int N_large = 5; // 고대 문명 전체 격자 크기입니다.
-    static final int N_small = 3; // 회전시킬 격자의 크기입니다.
+	
+	static int[][] board = new int[5][5];
+	static int K,M;
+	static Queue<Integer> wallNumber = new LinkedList<>();
+	static int[] dy = {0,1,0,-1};
+	static int[] dx = {1,0,-1,0};
+	
+	public static void main(String[] args) throws IOException {
+		
+		// System.setIn(new FileInputStream("src/eclipse/input.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		K = stoi(st.nextToken());
+		M = stoi(st.nextToken());
+		
+		// board 초기화
+		for(int i = 0; i < 5; i++) {
+			st = new StringTokenizer(br.readLine());
+			for(int j = 0; j < 5; j++) {
+				board[i][j] = stoi(st.nextToken());
+			}
+		}
+		
+		// 벽면 숫자 초기화
+		st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < M; i++) {
+			wallNumber.add(stoi(st.nextToken()));
+		}
+					
+//		int[][] temp = getRotatedMap(1,1,1);
+//		printBoard(temp);
+		
+		StringBuilder sb = new StringBuilder();
+		while (K-- > 0) {
+			
+			int maxScore = 0;
+			int[][] nextBoard = null;
+			
+			for(int cnt = 1; cnt <= 3; cnt++) {
+				for(int i = 0; i <= 2; i++) {
+					for(int j = 0; j <= 2; j++) {
+						int[][] rotatedBoard = getRotatedMap(i,j,cnt);
+						int score = getScore(rotatedBoard);
+//						System.out.println("i = " + i + " j = " + j + " cnt = " + cnt);
+//						printBoard(rotatedBoard);
+						if(score > maxScore) {
+							maxScore = score;
+							nextBoard = rotatedBoard;
+							
+						}
+					}
+				}
+			}
+			
+			if (nextBoard == null) {
+				break;
+			}
+			
+			board = nextBoard;
+			while(true) {
 
-    // 고대 문명 격자를 정의합니다
-    static class Board {
-        int[][] a = new int[N_large][N_large];
+				fillBoard();
+				int score = getScore(board);
+				if(score == 0) break;
+				maxScore += score;
+				
+			}
 
-        // 생성자입니다. 격자에 유물조각들을 초기화 합니다.
-        public Board() {
-            for (int i = 0; i < N_large; i++) {
-                for (int j = 0; j < N_large; j++) {
-                    a[i][j] = 0;
-                }
-            }
-        }
+			sb.append(maxScore + " ");
+		}
+		
+		System.out.println(sb.toString());
+	}
+	
+	private static int getScore(int[][] b) {
+		
+		int score = 0;
+		boolean[][] visited = new boolean[5][5];
+		
+		for(int i = 0; i < 5; i++) {
+			for(int j = 0; j < 5; j++) {
+				if (!visited[i][j]) {
+					
+					int v = b[i][j];
+					Queue<int[]> q = new LinkedList<>();
+					Queue<int[]> trace= new LinkedList<>();
+					
+					q.add(new int[] {i,j});
+					trace.add(new int[] {i,j});
+					visited[i][j] = true;
+					
+					while (!q.isEmpty()) {
+						int[] cur = q.poll();
+						
+						for (int k = 0; k < 4; k++) {
+							int ny = cur[0] + dy[k];
+							int nx = cur[1] + dx[k];
+							if (!inRange(ny,nx) || visited[ny][nx] || b[ny][nx] != v) continue;
+							
+							q.add(new int[] {ny,nx});
+							trace.add(new int[] {ny,nx});
+							visited[ny][nx] = true;
+						}
+					}
+					
+					if (trace.size() >= 3) {
+						
+						score += trace.size();
+						while(!trace.isEmpty()) {
+							int[] t = trace.poll();
+							b[t[0]][t[1]] = 0;
+						}
+					}
+					
+				}
+			}
+		}
+//		System.out.println("score = " + score);
+		return score;
+	}
 
-        // 주어진 y, x가 고대 문명 격자의 범위안에 있는지 확인하는 함수 입니다.
-        private boolean inRange(int y, int x) {
-            return 0 <= y && y < N_large && 0 <= x && x < N_large;
-        }
-
-        // 현재 격자에서 sy, sx를 좌측상단으로 하여 시계방향 90도 회전을 cnt번 시행했을때 결과를 return 합니다.
-        public Board rotate(int sy, int sx, int cnt) {
-            Board result = new Board();
-            for (int i = 0; i < N_large; i++) {
-                for (int j = 0; j < N_large; j++) {
-                    result.a[i][j] = this.a[i][j];
-                }
-            }
-            for (int k = 0; k < cnt; k++) {
-                // sy, sx를 좌측상단으로 하여 시계방향 90도 회전합니다.
-                int tmp = result.a[sy + 0][sx + 2];
-                result.a[sy + 0][sx + 2] = result.a[sy + 0][sx + 0];
-                result.a[sy + 0][sx + 0] = result.a[sy + 2][sx + 0];
-                result.a[sy + 2][sx + 0] = result.a[sy + 2][sx + 2];
-                result.a[sy + 2][sx + 2] = tmp;
-                tmp = result.a[sy + 1][sx + 2];
-                result.a[sy + 1][sx + 2] = result.a[sy + 0][sx + 1];
-                result.a[sy + 0][sx + 1] = result.a[sy + 1][sx + 0];
-                result.a[sy + 1][sx + 0] = result.a[sy + 2][sx + 1];
-                result.a[sy + 2][sx + 1] = tmp;
-            }
-            return result;
-        }
-
-        // 현재 격자에서 유물을 획득합니다.
-        // 새로운 유물 조각을 채우는것은 여기서 고려하지 않습니다.
-        public int calScore() {
-            int score = 0;
-            boolean[][] visit = new boolean[N_large][N_large];
-            int[] dy = {0, 1, 0, -1}, dx = {1, 0, -1, 0};
-
-            for (int i = 0; i < N_large; i++) {
-                for (int j = 0; j < N_large; j++) {
-                    if (!visit[i][j]) {
-                        // BFS를 활용한 Flood Fill 알고리즘을 사용하여 visit 배열을 채웁니다.
-                        // 이때 trace 안에 조각들의 위치가 저장됩니다.
-                        Queue<int[]> q = new LinkedList<>();
-                        Queue<int[]> trace = new LinkedList<>();
-                        q.offer(new int[]{i, j});
-                        trace.offer(new int[]{i, j});
-                        visit[i][j] = true;
-                        while (!q.isEmpty()) {
-                            int[] cur = q.poll();
-                            for (int k = 0; k < 4; k++) {
-                                int ny = cur[0] + dy[k], nx = cur[1] + dx[k];
-                                if (inRange(ny, nx) && a[ny][nx] == a[cur[0]][cur[1]] && !visit[ny][nx]) {
-                                    q.offer(new int[]{ny, nx});
-                                    trace.offer(new int[]{ny, nx});
-                                    visit[ny][nx] = true;
-                                }
-                            }
-                        }
-                        // 위에서 진행된 Flood Fill을 통해 조각들이 모여 유물이 되고 사라지는지 확인힙니다.
-                        if (trace.size() >= 3) {
-                            // 유물이 되어 사라지는 경우 가치를 더해주고 조각이 비어있음을 뜻하는 0으로 바꿔줍니다.
-                            score += trace.size();
-                            while (!trace.isEmpty()) {
-                                int[] t = trace.poll();
-                                a[t[0]][t[1]] = 0;
-                            }
-                        }
-                    }
-                }
-            }
-            return score;
-        }
-
-        // 유물 획득과정에서 조각이 비어있는 곳에 새로운 조각을 채워줍니다.
-        public void fill(Queue<Integer> que) {
-            // 열이 작고 행이 큰 우선순위로 채워줍니다.
-            for (int j = 0; j < N_large; j++) {
-                for (int i = N_large - 1; i >= 0; i--) {
-                    if (a[i][j] == 0 && !que.isEmpty()) {
-                        a[i][j] = que.poll();
-                    }
-                }
-            }
-        }
-    }
-
-    public static void main(String[] args) {
-        // 입력을 받습니다.
-        Scanner sc = new Scanner(System.in);
-        int K = sc.nextInt();
-        int M = sc.nextInt();
-        Queue<Integer> q = new LinkedList<>();
-        Board board = new Board();
-
-        for (int i = 0; i < N_large; i++) {
-            for (int j = 0; j < N_large; j++) {
-                board.a[i][j] = sc.nextInt();
-            }
-        }
-        for (int i = 0; i < M; i++) {
-            q.offer(sc.nextInt());
-        }
-
-        // 최대 K번의 탐사과정을 거칩니다.
-        while (K-- > 0) {
-            int maxScore = 0;
-            Board maxScoreBoard = null;
-            // 회전 목표에 맞는 결과를 maxScoreBoard에 저장합니다.
-            // (1) 유물 1차 획득 가치를 최대화
-            // (2) 회전한 각도가 가장 작은 방법을 선택
-            // (3) 회전 중심 좌표의 열이 가장 작은 구간을, 그리고 열이 같다면 행이 가장 작은 구간을 선택
-            for (int cnt = 1; cnt <= 3; cnt++) {
-                for (int sx = 0; sx <= N_large - N_small; sx++) {
-                    for (int sy = 0; sy <= N_large - N_small; sy++) {
-                        Board rotated = board.rotate(sy, sx, cnt);
-                        int score = rotated.calScore();
-                        if (maxScore < score) {
-                            maxScore = score;
-                            maxScoreBoard = rotated;
-                        }
-                    }
-                }
-            }
-            // 회전을 통해 더 이상 유물을 획득할 수 없는 경우 탐사를 종료합니다.
-            if (maxScoreBoard == null) {
-                break;
-            }
-            board = maxScoreBoard;
-            // 유물의 연쇄 획득을 위해 유물 조각을 채우고 유물을 획득하는 과정을 더이상 획득할 수 있는 유물이 없을때까지 반복합니다.
-            while (true) {
-                board.fill(q);
-                int newScore = board.calScore();
-                if (newScore == 0) break;
-                maxScore += newScore;
-            }
-
-            System.out.print(maxScore + " ");
-        }
-    }
+	private static int[][] getRotatedMap(int y, int x, int cnt) {
+		
+		int[][] nBoard = new int[5][5];
+		int[][] temp = new int[3][3];
+		
+		for(int i = 0; i < 5; i++) {
+			for(int j = 0; j < 5; j++) {
+				nBoard[i][j] = board[i][j];
+			}
+		}
+		
+		for (int k = 1; k <= cnt; k++) {
+			for (int i = 0; i <= 2; i++) {
+				for (int j = 0; j <= 2; j++) {					
+					temp[i][j] = nBoard[y+i][x+j];
+				}
+			}
+			
+			for(int i = 0; i <= 2 ; i++) {
+				for(int j = 0; j <= 2; j++) {
+					nBoard[y+i][x+j] = temp[2-j][i];
+				}
+			}
+		}
+		
+		return nBoard;
+	}
+	
+	private static void fillBoard() {
+		
+		for(int j = 0; j < 5; j++) {
+			for(int i = 4; i >= 0; i--) {
+				if(board[i][j] == 0 && !wallNumber.isEmpty()) {
+					board[i][j] = wallNumber.poll();
+				}
+			}
+		}
+	}
+	
+	private static void printBoard(int[][] b) {
+		System.out.println("==============");
+		for (int i = 0; i < 5; i++) {
+			for (int j = 0; j< 5; j++) {
+				System.out.print(b[i][j] + " ");
+			}
+			System.out.println("");
+		}
+		System.out.println("==============");
+	}
+	
+	
+	private static boolean inRange(int y, int x) {
+		return 0 <= y && y < 5 && 0 <= x && x < 5;
+	}
+	
+	private static int stoi(String s) {
+		return Integer.parseInt(s);
+	}
 }
